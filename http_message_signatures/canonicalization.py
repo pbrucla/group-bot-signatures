1edf4881f4ed362a447b18247db6a802a30b257a
kwong05 <kevin.g.wong.05@gmail.com>
2025-07-07 16:23:22 -0700

    un-delete some accidentally deleted code

--- http_message_signatures/canonicalization.py
+++ http_message_signatures/canonicalization.py
@@ -27,3 +27,16 @@ def normalize_headers(raw):
         new_dict[new_key] = new_value
 
     return new_dict
+
+
+def serialize_field(name, value):
+    # returns a single component line like "\"name\": value"
+    #name must already be lowercased and quoted
+    #TODO: implement field serialization
+    return ""
+
+def join_components(lines):
+    # lines: list of those serialized fields
+    # join with "\n", maybe trailing "\n", then ascii-encode
+    # TODO: implement component joining
+    return b""
