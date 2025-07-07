eeda14fa38881b74ec54e625ecbc3eb9ab963d8f
smiles1313 <46733232+smiles1313@users.noreply.github.com>
2025-07-07 15:06:08 -0700

    query_param func

--- http_message_signatures/components.py
+++ http_message_signatures/components.py
@@ -1,4 +1,4 @@
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs     # for parsing urls & query strings
 
 # fixed derived-component extractors
 DERIVED = {
@@ -12,8 +12,21 @@ DERIVED = {
                         + (("?" + urlparse(r.url).query) if urlparse(r.url).query else ""),
 }
 
-def query_param(request, name):
-    # handles '@query-param';name="param"
-    #parse request.url  and return first value of ?param
-    # TODO: impl query param extraction
-    return ""
+
+#parses request.url  and return first value of ?param
+def query_param(request, name):   # handles '@query-param';name="param"
+
+    q_params = parse_qs(urlparse(request.url).query)   # dict returned
+    val = q_params.get(name, "invalid")  # returns invalid if name (key) not found
+    return val
+
+
+#----------------- Testing ----------------------
+
+class MockRequest:
+    def __init__(self, url):
+        self.url = url
+
+r = MockRequest("https://www.example.com/product?affiliate_id=56789")
+print(query_param(r, "affiliate_id"))   # prints 56789
+print(query_param(r, "name"))   # prints invalid
