37907935ffaf2c47a4eca0cd5d4cd254a9bb994f
kwong05 <kevin.g.wong.05@gmail.com>
2025-07-10 03:45:15 -0700

    housekeeping (comments, grouping code), modify whitespace stripping (assumes assumptions are correct)

--- http_message_signatures/canonicalization.py
+++ http_message_signatures/canonicalization.py
@@ -7,27 +7,16 @@ def normalize_headers(raw):
     new_dict = {}
 
     for key in raw: # iterate through dictionary
-        # normalize the key (lowercase)
-        new_key = key.lower()
+        # normalize the key (lowercase, remove any whitespace before it)
+        key_lowered = key.lower()
+        new_key = re.sub(r"^\s+", r"", key_lowered)
 
         # get value
         value = raw[key]
 
-        try: # strip whitespace (around colons wrapping a base64-encoded value)
-            # find colon-wrapped base64 (RFC 4648), if there is one
-            original_colon_b64 = re.search(r"\s*:\s*[A-Za-z0-9+/=]{64}\s*:\s*", value)
-
-            if (original_colon_b64): # there is one
-                # remove whitespace around colons
-                new_colon_b64 = re.sub(r"\s*:\s*", r":", original_colon_b64[0])
-
-                # substitute in
-                value_stripped_ws = re.sub(r"\s*:\s*[A-Za-z0-9+/=]{64}\s*:\s*", new_colon_b64, value)
-            
-            else: # there isn't one; do nothing
-                value_stripped_ws = value
-
-        except TypeError: # handle cases where values aren't strings (also do nothing)
+        try: # normalize the value (remove any whitespace after it)
+            value_stripped_ws = re.sub(r"\s+$", r"", value)
+        except TypeError: # handle cases where values aren't strings (do nothing)
             value_stripped_ws = value
 
         if (new_key in new_dict): # key already exists in new_dict; merge values
