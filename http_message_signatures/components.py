fc2fc1201f1bb2a71ee214a8c5822fab04fbd3d1
smiles1313 <46733232+smiles1313@users.noreply.github.com>
2025-07-14 14:18:17 -0700

    Update components.py

--- http_message_signatures/components.py
+++ http_message_signatures/components.py
@@ -1,19 +1,31 @@
-from urllib.parse import urlparse, parse_qs
+from urllib.parse import urlparse, parse_qs     # for parsing urls & query strings
 
 # fixed derived-component extractors
 DERIVED = {
-    "@method"        : lambda r: r.method,
-    "@scheme"        : lambda r: urlparse(r.url).scheme,
-    "@authority"     : lambda r: urlparse(r.url).netloc,
-    "@target-uri"    : lambda r: r.url,
-    "@path"          : lambda r: urlparse(r.url).path or "/",
-    "@query"         : lambda r: "?" + urlparse(r.url).query if urlparse(r.url).query else "",
-    "@request-target": lambda r: (urlparse(r.url).path or "/")
-                        + (("?" + urlparse(r.url).query) if urlparse(r.url).query else ""),
+   "@method"        : lambda r: r.method,
+   "@scheme"        : lambda r: urlparse(r.url).scheme,
+   "@authority"     : lambda r: urlparse(r.url).netloc,
+   "@target-uri"    : lambda r: r.url,
+   "@path"          : lambda r: urlparse(r.url).path or "/",
+   "@query"         : lambda r: "?" + urlparse(r.url).query if urlparse(r.url).query else "",
+   "@request-target": lambda r: (urlparse(r.url).path or "/")
+                       + (("?" + urlparse(r.url).query) if urlparse(r.url).query else ""),
 }
 
-def query_param(request, name):
-    # handles '@query-param';name="param"
-    #parse request.url  and return first value of ?param
-    # TODO: impl query param extraction
-    return ""
+#parses request.url  and return first value of ?param
+def query_param(request, name):   # handles '@query-param';name="param"
+   q_params = parse_qs(urlparse(request.url).query)   # dict returned
+   val = q_params.get(name, "invalid")  # returns invalid if name (key) not found
+   return val
+
+
+
+#---------------------------- Testing --------------------------------
+class MockRequest:
+   def __init__(self, url):
+       self.url = url
+
+
+r = MockRequest("https://www.example.com/product?affiliate_id=56789")
+print(query_param(r, "affiliate_id"))   # prints 56789
+print(query_param(r, "name"))   # prints invalid
