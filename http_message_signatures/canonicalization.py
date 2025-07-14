8aac8ba3e3ec03a5cd76b7e80a80f0cde3b19562
kwong05 <kevin.g.wong.05@gmail.com>
2025-07-14 12:42:15 -0700

    simplify whitespace stripping and general logic

--- http_message_signatures/canonicalization.py
+++ http_message_signatures/canonicalization.py
@@ -1,4 +1,3 @@
-import re
 def normalize_headers(raw):
     #dict of header-name -> value
     # lower-case names, strip whitespace, merge duplicates with ", "
@@ -7,26 +6,21 @@ def normalize_headers(raw):
     new_dict = {}
 
     for key in raw: # iterate through dictionary
-        # normalize the key (lowercase, remove any whitespace before it)
-        key_lowered = key.lower()
-        new_key = re.sub(r"^\s+", r"", key_lowered)
+        # normalize the key (lowercase, strip whitespace)
+        new_key = key.lower().strip()
 
         # get value
         value = raw[key]
 
-        try: # normalize the value (remove any whitespace after it)
-            value_stripped_ws = re.sub(r"\s+$", r"", value)
-        except TypeError: # handle cases where values aren't strings (do nothing)
+        try: # normalize the value (strip whitespace)
+            value_stripped_ws = value.strip(" \t")
+        except: # handle cases where values aren't strings (do nothing)
             value_stripped_ws = value
 
         if (new_key in new_dict): # key already exists in new_dict; merge values
-            existing_value = new_dict[new_key]
-            new_value = str(existing_value) + ", " + str(value_stripped_ws)
+            new_dict[new_key] = str(new_dict[new_key]) + ", " + str(value_stripped_ws)
         else: # key does not yet exist
-            new_value = value_stripped_ws
-            
-        # update
-        new_dict[new_key] = new_value
+            new_dict[new_key] = value_stripped_ws
 
     return new_dict
 
