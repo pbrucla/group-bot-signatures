896dfef60e211b7053fff0763c9a2688135aed32
kwong05 <kevin.g.wong.05@gmail.com>
2025-07-23 14:42:41 -0700

    create_challenge_hash: accept messages as an argument

--- bbs/sage/util.py
+++ bbs/sage/util.py
@@ -57,8 +57,9 @@ def bytes_to_E2_point(b):
         return points[0] if (larger and select) or (not larger and not select) else points[1]
     return E2(Fp2(int.from_bytes(b[:Lbytes], "big")) * u + int.from_bytes(b[Lbytes:2*Lbytes], "big"), Fp2(int.from_bytes(b[2*Lbytes:3*Lbytes], "big")) * u + int.from_bytes(b[3*Lbytes:], "big"))
 
-def create_challenge_hash(T1, T2, T3, R1, R2, R3, R4, R5):
+def create_challenge_hash(msg, T1, T2, T3, R1, R2, R3, R4, R5):
     h = hashlib.sha256()
+    h.update(str(msg).encode())
     h.update(E1_point_to_bytes(T1))
     h.update(E1_point_to_bytes(T2))
     h.update(E1_point_to_bytes(T3))
