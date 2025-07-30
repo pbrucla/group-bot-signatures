225e67294ef269407552e36aae21e975d0627dc2
Roshni Dave <79119564+roshda@users.noreply.github.com>
2025-07-30 16:00:28 -0700

    got the signature to verify and added a debug script

--- bbs/sage/util.py
+++ bbs/sage/util.py
@@ -1,7 +1,7 @@
 from BLS12381 import *
 import base64, hashlib
 
-Lbytes = (p.nbits() + 7) // 8
+Lbytes = (p.nbits() + 7)//8
 
 def b64(b): return base64.urlsafe_b64encode(b).rstrip(b'=').decode()
 def int_to_bytes(x, L=Lbytes): return int(x).to_bytes(L, 'big')
@@ -55,8 +55,8 @@ def bytes_to_E2_point(b):
         points = E2.lift_x(x, all=True)
         select = poly_to_bytes(points[0][1]) > poly_to_bytes(points[1][1])
         return points[0] if (larger and select) or (not larger and not select) else points[1]
-    return E2(Fp2(int.from_bytes(b[:Lbytes], "big")) * u + int.from_bytes(b[Lbytes:2*Lbytes], "big"),
-              Fp2(int.from_bytes(b[2*Lbytes:3*Lbytes], "big")) * u + int.from_bytes(b[3*Lbytes:], "big"))
+    return E2(Fp2(int.from_bytes(b[:Lbytes], "big")) * u + int.from_bytes(b[Lbytes:2*Lbytes], "big"), Fp2(int.from_bytes(b[2*Lbytes:3*Lbytes], "big")) * u + int.from_bytes(b[3*Lbytes:], "big"))
+
 
 def create_challenge_hash(msg, T1, T2, T3, R1, R2, R3, R4, R5):
     h = hashlib.sha256()
@@ -69,6 +69,4 @@ def create_challenge_hash(msg, T1, T2, T3, R1, R2, R3, R4, R5):
     h.update(poly_to_bytes(R3))
     h.update(E1_point_to_bytes(R4))
     h.update(E1_point_to_bytes(R5))
-    # FIX: return challenge reduced modulo r (group order)
-    c_big = int.from_bytes(h.digest(), "big") % r
-    return ZZ(c_big)
+    return Fp(int.from_bytes(h.digest(), "big"))
